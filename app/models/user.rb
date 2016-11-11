5917b66e556603b1c4b097e9c47d33f193c33b07
Ly Duc Long <longlyduc@gmail.com>
2016-11-11 17:26:49 +0000

    show entries

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,7 @@ class User < ActiveRecord::Base
 	def feed
 		following_ids = "SELECT followed_id FROM relationships
                      WHERE  follower_id = :user_id"
-    	Micropost.where("user_id IN (:following_ids) OR user_id = :user_id",
+    	Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id",
                     following_ids: following_ids, user_id: id)
   	end
 
