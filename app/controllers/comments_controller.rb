033ddd20948b69a341bda9407f5f5ba07eb52397
Ly Duc Long <longlyduc@gmail.com>
2016-11-11 19:01:03 +0000

    finish demo

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,8 @@ class CommentsController < ApplicationController
 
 	def create
     @comment=Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.micropost_id = params[:micropost_id]
     if @comment.save
       redirect_to :back
     end
