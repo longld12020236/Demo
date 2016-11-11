149135eabdead7c48bab7462a042f0b8c60a797e
Ly Duc Long <longlyduc@gmail.com>
2016-11-11 17:13:33 +0000

    fix comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,15 @@
 class CommentsController < ApplicationController
 	before_action :logged_in_user, only: [:create, :destroy]
 
-  def new
-    @comment=Comment.all
-  end
-
-  def create
-    @comment = Comment.new(comment_params)
+	def create
+    @comment=Comment.new(comment_params)
     if @comment.save
       redirect_to :back
     end
+  end
+
+  def new
+    @comment=Comment.all
   end
 
     private
